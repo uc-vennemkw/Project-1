a35081c3f6e7eefb8fb65e5eae8d75561c138491
uc-vennemkw <58619751+uc-vennemkw@users.noreply.github.com>
2021-02-21 16:44:08 -0500

    Update Project 1.ps1

--- Project 1.ps1
+++ Project 1.ps1
@@ -3,10 +3,6 @@
 # The original had the format at dd/MM/yyyy and the time (HH:mm) in 24 hour with UTC time offput
 # I changed it from 24 hour time (HH:mm) to 12 hour time (h:mm) with the lowercase tt to add the am/pm tag at the end
 
-# To run the script, you can either copy and paste the code into powershell ISE or, create a notepad with the code and save the file as a .ps1. 
-# After saving it, you can right click it and select "Run with powershell" and it will populate in powershell.
-
-
 Get-Date -Format "dddd MM/dd/yyyy h:mm tt"
 
 
