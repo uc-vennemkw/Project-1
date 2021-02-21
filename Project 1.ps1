fd7bef3e02ed9d80ec9e2c12a3c0399e48546cfe
uc-vennemkw <58619751+uc-vennemkw@users.noreply.github.com>
2021-02-21 16:54:58 -0500

    Update Project 1.ps1

--- Project 1.ps1
+++ Project 1.ps1
@@ -1,9 +1,4 @@
-
-# Cited work from Microsoft docs on powershell on proper formatting
-# The original had the format at dd/MM/yyyy and the time (HH:mm) in 24 hour with UTC time offput
-# I changed it from 24 hour time (HH:mm) to 12 hour time (h:mm) with the lowercase tt to add the am/pm tag at the end
-
-Get-Date -Format "dddd MM/dd/yyyy h:mm tt"
+Get-Date -Format "dddd MMMM dd, yyyy h:mm:ss tt"
 
 
 
